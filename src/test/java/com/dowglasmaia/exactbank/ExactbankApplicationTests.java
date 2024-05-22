8248bace3e36f60fb75211abc2365c835ba1a81b
Dowglas Maia <dowglasmaia@gmail.com>
2024-05-22 17:19:48 -0300

    create swagger and configuration  pom

---
+++ src/test/java/com/dowglasmaia/exactbank/ExactbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dowglasmaia.exactbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExactbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
